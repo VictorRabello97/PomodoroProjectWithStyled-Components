985887e1066a246c1f11782cb65865a0da9ced28
Victor Rabello <109052634+VictorRabello97@users.noreply.github.com>
2022-11-21 18:10:48 -0300

    Implementando rotas para o código e adicionando Header Default

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,17 @@
 import {Routes, Route} from 'react-router-dom'
-import { History } from './pages/history'
+import { DefaultLayout } from './layouts/DefaultLayout'
+import { History } from './pages/History'
 
-import { Home } from './pages/home'
+import { Home } from './pages/Home'
 
 export function Router(){
     return (
         
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/history" element={<History />} />
-
+            <Route path="/" element={<DefaultLayout />} >
+                <Route path="/" element={<Home />} />
+                <Route path="/history" element={<History />} />
+            </Route>
         </Routes>
 
     )
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+import { HeaderContainer } from "./Styles";
+import logo from "../../assets/logo.svg"
+import { Timer, Scroll } from "phosphor-react";
+import { NavLink } from "react-router-dom";
+
+
+export function Header(){
+    return (
+        <HeaderContainer>
+
+            <img src={logo} alt="" />
+            <nav>
+                <NavLink to="/">
+                    <Timer size={24}/>
+                </NavLink>
+
+                <NavLink to="/history"> 
+                    <Scroll size={24} />
+                </NavLink>
+
+
+
+            </nav>
+
+        </HeaderContainer>
+
+    )}
